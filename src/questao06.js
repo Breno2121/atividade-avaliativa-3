2f468499b1a956732a991de9c861c08f58264558
JOSE BRENO OLIVEIRA DA SILVA <jose_bo_silva@estudante.sc.senai.br>
2024-06-18 22:00:42 -0300

    atividade avaliativa

--- src/questao06.js
+++ src/questao06.js
@@ -2,5 +2,8 @@ import leia from "readline-sync"
 
 var ano = leia.questionInt("Digite o ano desejado: ");
 
-
-//% 100 == 0;
+if (ano % 100 == 0 || ano % 400 == 0 ){
+    console.log("Este ano nao e bisexto");
+}else if(ano % 4 == 0 || ano % 100 == 0){
+    console.log("O ano e bisexto");
+}
